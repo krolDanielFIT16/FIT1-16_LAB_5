9f8ae400c61a5e15bc1e4800c0588d2814159630
krolDanielFIT16 <krol.daniel.2005@gmail.com>
2022-09-19 22:40:24 +0300

    fix

--- p1.py
+++ p1.py
@@ -2,7 +2,11 @@ from math import *
 
 
 def func(x):
-    return (2.27*pow(e, 4*x+1)+3) if x > 7 else ((0.64*pow(x, x+0.1)) if (0.5 < x <= 7) else (log(fabs(x - e ** x), e)))
-
-
+    if x > 7:
+        return 2.27*pow(e, 4*x+1)+3
+    elif 0.5 < x <= 7:
+        return 0.64*pow(x, x+0.1)
+    else:
+        return log(fabs(x - e ** x), e)
+    
 print(func(float(input("x = "))))
